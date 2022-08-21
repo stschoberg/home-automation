3e88319475e4d4a03e6466c5792394a59dd50191
Sam Schoberg <samschoberg@Sams-MacBook-Pro-3.local>
2022-08-21 13:48:16 -0400

    first commit

---
+++ index.ts
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const axios = require('axios').default;
+
+async function updateLightState(light: number, state: any) {
+    try {
+        await axios({
+            method: 'put',
+            url: `http://192.168.1.172/api/${process.env.HUE_USER}/lights/${light}/state`,
+            headers: {}, 
+            data: state
+    })
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+async function getAllQueries(hostname: string) {
+    try {
+        const response = await axios.get(`http://pi.hole/admin/api.php?getAllQueries&auth=${process.env.PIHOLE_TOKEN}`);
+        return response.data.data.filter(req => req[3] == hostname)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function main(){
+    const queries = await getAllQueries(process.env.HOSTNAME!)
+    const mostRecentQuery = queries[queries.length - 1];
+    const mostRecentQueryTimestamp = new Date(mostRecentQuery[0] * 1000)
+    const isActive =  Math.abs((new Date()).valueOf() - mostRecentQueryTimestamp.valueOf()) < 1000 * 60 * 5;
+    if(isActive) {
+        console.log("Active")
+        await updateLightState(3, {on: true})
+    } else {
+        await updateLightState(3, {on: false})
+    }
+
+}
+
+main()
