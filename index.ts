dbcba52d71d66de07fe8ce4e7ae29a3241e16933
Sam Schoberg <samschoberg@Sams-MacBook-Pro-3.local>
2022-08-21 14:32:19 -0400

    winston logging

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 require('dotenv').config()
 const axios = require('axios').default;
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(
+    timestamp(),
+    myFormat
+  ),
+  transports: [new transports.Console(), 
+    new transports.File({
+      filename: './log',
+      level: 'info'
+    }),]
+});
 
 async function updateLightState(light: number, state: any) {
     try {
@@ -11,7 +29,7 @@ async function updateLightState(light: number, state: any) {
     })
     }
     catch (error){
-        console.log(error);
+        logger.error(error);
     }
 }
 
@@ -20,7 +38,7 @@ async function getAllQueries(hostname: string) {
         const response = await axios.get(`http://pi.hole/admin/api.php?getAllQueries&auth=${process.env.PIHOLE_TOKEN}`);
         return response.data.data.filter(req => req[3] == hostname)
     } catch (error) {
-        console.error(error);
+        logger.error(error);
     }
 }
 
@@ -29,12 +47,12 @@ async function main(){
     const mostRecentQuery = queries[queries.length - 1];
     const mostRecentQueryTimestamp = new Date(mostRecentQuery[0] * 1000)
     const isActive =  Math.abs((new Date()).valueOf() - mostRecentQueryTimestamp.valueOf()) < 1000 * 60 * 5;
-    console.log(`${process.env.HOSTNAME} last activity: ${mostRecentQueryTimestamp}`)
+    logger.info(`${process.env.HOSTNAME} last activity: ${mostRecentQueryTimestamp}`)
     if(isActive) {
-        console.log(`${process.env.HOSTNAME} is active on the network`)
+        logger.info(`${process.env.HOSTNAME} is active on the network`)
         await updateLightState(3, {on: true})
     } else {
-        console.log(`${process.env.HOSTNAME} is inactive on the network`)
+        logger.info(`${process.env.HOSTNAME} is inactive on the network`)
         await updateLightState(3, {on: false})
     }
 
